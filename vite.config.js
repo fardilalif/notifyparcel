2ec26bbd582057142a5dece6ce2e40350209dc05
fardilalif <muhdfardilalif@gmail.com>
2024-02-01 12:37:10 +0800

    fixed pwa

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { icons } from "./manifest.json";
 
 const manifestForPlugin = {
   registerType: "prompt",
-  includeAssets: ["./public/favicon.ico"],
+  includeAssets: [".//favicon.ico"],
   manifest: {
     name: "NotifyParcel",
     short_name: "Notify Parcel",
     description: "A parcel management system",
-    icon: [
-      {
-        src: "public/manifest-icon-192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "public/manifest-icon-192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "public/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "public/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-    ],
+    icon: icons,
     display: "standalone",
     scope: "/",
     start_url: "/",
